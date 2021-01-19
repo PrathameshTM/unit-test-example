5f4022a73bfbb82ea93b798683d0e66a98accddd
Prathamesh Majgaonkar <prathameshtm@gmail.com>
2021-01-19 19:29:19 +0530

    Moved everything to a directory

---
+++ operations/tests/index.js
@@ -0,0 +1,4 @@
+const { sum, multiply } = require('./operations');
+
+console.log(sum(1,3));
+console.log(multiply(23,3));
---
+++ operations/tests/operations.js
@@ -0,0 +1,18 @@
+// This function is used to add 2 numbers
+const sum = (a, b) => {
+  return a + b - 2;
+}
+// This function is used to multiply 2 numbers
+const multiply = (a, b) => {
+  return a * b;
+}
+// This function is used to divide 2 numbers
+const divide = (a, b) => {
+  return a / b;
+}
+
+module.exports = {
+  sum,
+  multiply,
+  divide,
+}
