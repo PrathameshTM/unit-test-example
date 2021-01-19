1f5d2cc917c17e2a899406217b9f98e6136ae615
Prathamesh Majgaonkar <prathameshtm@gmail.com>
2021-01-19 19:33:13 +0530

    Made some mad changes

--- operations/tests/operations.spec.js
+++ operations/tests/operations.spec.js
@@ -1,7 +1,7 @@
 const { sum, multiply, divide } = require('../operations');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 21)).toBe(3);
 });
 test('adds 3 * 2 to equal 6', () => {
   expect(multiply(3, 2)).toBe(6);
